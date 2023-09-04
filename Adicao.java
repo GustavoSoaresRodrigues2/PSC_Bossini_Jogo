f09d11225acb46a8528be4d5067400485c4569e7
Your Name <you@example.com>
2023-09-04 19:52:12 -0300

    aula(input-output): declarar variaveis

--- Adicao.java
+++ Adicao.java
@@ -3,21 +3,25 @@ import java.util.Scanner;
 public class Adicao {
     public static void main(String [] args) {
         // Declarar variável
-        // reserva um pedaço de memória para usarmos quando querem reutiliza-la
-        // double n1;
-        // double n2;
-        // double result;
+        // reserva um pedaço de memória para usarmos quando queremos reutiliza-la
+        double n1;
+        double n2;
+        double result;
 
         // Entrada / System.in é em referência a entrada padrão (Teclado)
         Scanner sc = new Scanner(System.in);
-
         
+        // Pegando valores
         System.out.print("Primeiro número: ");
-        double n1 = sc.nextInt();
-        
+        n1 = sc.nextDouble();
+
         System.out.print("Segundo número: ");
-        double n2 = sc.nextInt();
-        double result = n1 + n2;
+        n2 = sc.nextDouble();
+
+        // Processo
+        result = n1 + n2;
+
+        // Saída
         System.out.print("A Soma é: " + result);
 
     }
